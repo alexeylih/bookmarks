26ed9e8574743fc5e504a7d2514a6242b40afbf4
sommar <alexeymn@gmail.com>
2014-09-17 00:48:51 +0300

    Changed Redis connection

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,8 @@
 require 'resque_scheduler'
+
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:d5f4522278848837dedb0a97c1ba65da@barreleye.redistogo.com:11855/"
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+
 Resque.schedule = YAML.load_file(File.join(Rails.root, 'config/resque_scheduler.yml'))
