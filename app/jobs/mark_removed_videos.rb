a5e6811037720d62984d0030dc49a65cd6410696
sommar <alexeymn@gmail.com>
2014-09-19 01:13:41 +0300

    Added .travis.yml and changes in resque configuration

--- app/jobs/mark_removed_videos.rb
+++ app/jobs/mark_removed_videos.rb
@@ -4,6 +4,7 @@ class MarkRemovedVideosJob
   def self.perform
   	YoutubeBookmark.where(removed: false).each do |bookmark|
 		begin
+			sleep 2
 			YouTubeIt::Client.new.video_by(bookmark.url)
 		rescue Exception => e
 		  	if e.to_s == "403" #Forbidden 
